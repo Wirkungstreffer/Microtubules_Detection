12dd23d665166b3f331b353393c1a70b4e240cd4
ZiXuan Liu <35823389+Wirkungstreffer@users.noreply.github.com>
2020-12-13 01:13:26 +0100

    Update Microtubules_Detection.py

--- Microtubules_Detection.py
+++ Microtubules_Detection.py
@@ -11,12 +11,6 @@ import cv2
 def midpoint(ptA, ptB):
 	return ((ptA[0] + ptB[0]) * 0.5, (ptA[1] + ptB[1]) * 0.5)
 
-# parameters
-#ap = argparse.ArgumentParser()
-#ap.add_argument("-i", "--image", required=True, help="path to the input image")
-#ap.add_argument("-w", "--width", type=float, required=True, help="width of the left-most object in the image (in inches)")
-#args = vars(ap.parse_args())
-
 # read imgae
 image = cv2.imread("1.png")
 cv2.imshow("image", image)
