9c716c35f53d8064e47831e69e44aba1af169b9c
ZiXuan Liu <35823389+Wirkungstreffer@users.noreply.github.com>
2021-04-29 16:35:11 +0200

    Add files via upload

---
+++ Semantic_Segmentation/training_data/json.file/json_to_dataset.sh
@@ -0,0 +1,12 @@
+#！/bin/bash
+let i=1
+path=./
+cd ${path}
+for file in *.json
+do
+     labelme_json_to_dataset ${file}
+     base=$(echo ${file} | sed 's/\.json//g')
+     mv ${base}_json/label.png ${base}_label.png
+     mv ${base}_json/img.png ${base}_img.png
+     let i=i+1
+ done
