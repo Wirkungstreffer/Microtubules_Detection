c0c28e0368876b212143181fe40105c3b8b3127f
ZiXuan Liu <35823389+Wirkungstreffer@users.noreply.github.com>
2021-05-08 18:46:17 +0200

    Add files via upload

--- Semantic_Segmentation/training_data/Divide_Data_for_Training_and_Testing.py
+++ Semantic_Segmentation/training_data/Divide_Data_for_Training_and_Testing.py
@@ -3,31 +3,44 @@ import random
 import shutil
 import glob
 import cv2
+import numpy as np
 
-
-
+#### The functions of this script is to split training and testing dataset ####
+#### Total dataset has 26 sequence, each sequence has 12 images, this scrpit will randomly choose "seq" numbers of sequcences into testing dataset ####
+#### The images names of training dataset and testing dataset will be stored into "training_data.txt" and "testing_data.txt" respectively ####
 
 # Define a images loading function
-def load_images(image_file_directory):
-    # Create a image list
-    images = []
+def delete_end_str(path):
 
-    # Check if the input folder exist
-    if os.path.exists(image_file_directory)==False:
-        raise FileNotFoundError( 'No such file or directory:'+ image_file_directory)
+  # Check if the input folder exist
+  if os.path.exists(path)==False:
+    raise FileNotFoundError( 'No such file or directory:'+ path)
+  
+  # Create image path list and image name list
+  list_img_name = []
+  list_read_img = []
+  
+  # Start reading image path and name
+  filelist = os.listdir(path)
+  for filename in filelist:
+    filename = path + filename
+    
+    # Add image name to list
+    list_img_name.append(filename)
+    
+    # Make sure the names is in order
+    list_img_name.sort()
 
-    # Reading the images in the folder 
-    for directory_path in glob.glob(image_file_directory):
-        image_path = glob.glob(os.path.join(directory_path, '*.png'))
-
-        # Make sure reading sequence of the images is correctly according to the name sequence of images
-        image_path.sort()
-
-        # Reading images, dd up into images list
-        for i in image_path:     
-            images.append(i)
-
-    return images
+    # Get the image path
+    new_file_name = filename.split(".png")[0]
+    
+    # Add image path to list
+    list_read_img.append(new_file_name)
+    
+    # Make sure the path is in order
+    list_read_img.sort()
+  
+  return list_img_name, list_read_img
 
 
 # Set the total images and labels folder path
@@ -39,31 +52,78 @@ image_test_path = "Semantic_Segmentation/training_data/image_test/"
 label_test_path = "Semantic_Segmentation/training_data/label_test/" 
 
 # Load images and labels
-images = load_images(images_path)
-labels = load_images(labels_path)
+list_image_name, list_read_image = delete_end_str(images_path)
+list_label_name, list_read_label = delete_end_str(labels_path)
 
-# Set the number of test set images
-k=37 
+# Each sequence has 12 images
+image_quantity_each_sequence = 12
+
+# Total sequence quantity in the hole dataset: 26
+total_group_number = int(len(list_read_image)/image_quantity_each_sequence)
+
+# Create image sequence list
+images_sequence = []
+
+# Read each 12 images, save them as sublist of image sequence list
+for img in range(0,len(list_read_image),image_quantity_each_sequence):
+  image_group = list_read_image[img:img+image_quantity_each_sequence]
+  images_sequence.append(image_group)
+
+# Create label sequence list
+labels_sequence = []
+
+# Read each 12 label, save them as sublist of label sequence list
+for lab in range(0,len(list_read_label),image_quantity_each_sequence):
+  label_group = list_read_label[lab:lab+image_quantity_each_sequence]
+  labels_sequence.append(label_group)
+
+
+# Set the sequence number of test set images
+seq = 3 
 
 # Creat a number list
-number = []
+seq_number = []
 
-if k > len(images):
+if seq > total_group_number:
 
-  # The test set number should smaller than total image number
-  print("The input number is larger than totall images quantity")
+  # The test set sequence number should smaller than total image sequence number
+  print("The input sequence number is larger than totall sequence quantity")
 else:
 
-  # Non-repeatable randomly selet test set images quantity k, store them in the number list
-  number = random.sample(range(0, len(images)-1), k)
+  # Non-repeatable randomly selet test set images sequence quantity, store them in the seq_number list
+  seq_number = random.sample(range(0, total_group_number-1), seq)
 
+# Define a file moving function which separate training and testing data set
+def move_sequence_into_testset(seq_number):
+  for num in seq_number: 
+    i=0
+    # Select the images and labels correspending to the sequence number list
+    sel_group_image = images_sequence[num]
+    sel_group_label = labels_sequence[num]
 
-for num in number: 
+    # Move the seleted images and labels in to test set folder
+    while i < len(sel_group_image):
+      shutil.move((images_sequence[num][i]+'.png'), image_test_path)
+      shutil.move((labels_sequence[num][i]+'.png'), label_test_path)
+      i = i + 1
 
-  # Select the images and labels correspending to the number list
-  image = images[num]
-  label = labels[num]
+# Implement dividing procedure
+move_sequence_into_testset(seq_number)
 
-  # Move the seleted images and labels in to test set folder
-  shutil.move(image, image_test_path)
-  shutil.move(label, label_test_path)
+# Reading train dataset informations
+training_image_name, training_image_path = delete_end_str(images_path)
+
+# Save the training image names into "training_data.txt"
+for train_file_name in training_image_path:
+  train_file_name = train_file_name.strip("Semantic_Segmentation/training_data/images/")
+  with open("Semantic_Segmentation/training_data/training_data.txt","a") as file:
+    file.write(train_file_name + "\n")
+
+# Reading test dataset informations
+testing_image_name, testing_image_path = delete_end_str(image_test_path)
+
+# Save the testing names into "training_data.txt"
+for test_file_name in testing_image_path:
+  test_file_name = test_file_name.strip("Semantic_Segmentation/training_data/images/")
+  with open("Semantic_Segmentation/training_data/testing_data.txt","a") as file:
+    file.write(test_file_name + "\n")
